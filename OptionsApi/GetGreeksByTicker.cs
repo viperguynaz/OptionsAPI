b28e792cc71a8637ac5604d80c83c9d2dc0cc92f
viperguynaz <viperguynaz@gmail.com>
2023-02-14 06:54:59 -0700

    integrate eodhistoricaldata Options Data API

--- OptionsApi/GetGreeksByTicker.cs
+++ OptionsApi/GetGreeksByTicker.cs
@@ -1,55 +1,78 @@
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Reflection;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using static OptionsApi.EodOptions;
 
 namespace OptionsApi
 {
     public class GetGreeksByTicker
     {
         private readonly ILogger _logger;
-        private const string urlBase = "https://api.nasdaq.com/api/quote/";
-        private static HttpClient client;
+        private const string urlBase = "https://eodhistoricaldata.com/api";
+        private const string apiKey = "623b75cf0a9c85.30775135";
+        private static HttpClient client = new();
 
         public GetGreeksByTicker(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<GetGreeksByTicker>();
-            var socketsHandler = new SocketsHttpHandler
-            {
-                PooledConnectionLifetime = TimeSpan.FromMinutes(2)
-            };
-
-            client = new HttpClient(socketsHandler);
         }
 
         [Function("GetGreeksByTicker")]
         public async Task<HttpResponseData> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous,
             "get",
-            Route = "GetGreeksByTicker/{ticker:alpha:required}/{expiration:regex(^\\d{{4}}-\\d{{2}}-\\d{{2}}):required}")] HttpRequestData req,
+            Route = "GetGreeksByTicker/{ticker:alpha:required}")] HttpRequestData req,
             string ticker,
             string expiration)
         {
             _logger.LogInformation($"request for Ticker: {ticker} | Expiration: {expiration}");
-            var url = $"{urlBase}{ticker}/option-chain/greeks?assetclass=etf&date={expiration}";
+            var url = $"{urlBase}/options/{ticker}.US?api_token={apiKey}&fmt=json";
 
+            var greeksResponse = await client.GetFromJsonAsync<EodOptionsResponse>(url);
 
-            //var greeksResponse = await client.GetFromJsonAsync<GreeksResponse>(url);
+            var csv = new StringBuilder();
 
-            using HttpResponseMessage greeksResponse = await client.GetAsync(url);
-            greeksResponse.EnsureSuccessStatusCode();
-            string responseBody = await greeksResponse.Content.ReadAsStringAsync();
+            foreach (PropertyInfo prop in greeksResponse.Data[0]?.Options.Put[0].GetType().GetProperties())
+            {
+                csv.AppendFormat("{0}, ", prop.Name);
+            }
+            csv.Remove(csv.Length - 2, 2);
+            csv.AppendLine();
+            foreach(var datum in greeksResponse.Data)
+            {
+                foreach (var put in datum.Options.Put ?? new List<EodOptions.Put>())
+                {
+                    foreach (PropertyInfo prop in put.GetType().GetProperties())
+                    {
+                        var type = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+                        if (type == typeof(double?))
+                        {
+                            csv.AppendFormat("{0, 0.0}, ", prop.GetValue(put));
+                        } else if (type == typeof(int?))
+                        {
+                            csv.AppendFormat("{0, 0}, ", prop.GetValue(put));
+                        } else 
+                        {
+                            var x = prop.GetValue(put);
+                            csv.AppendFormat("{0}, ", x);
+                        }
+
+                    }
+                    csv.Remove(csv.Length - 2, 2);
+                    csv.AppendLine();
+                }
+            }
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
-            response.WriteString(JsonSerializer.Serialize(greeksResponse));
-
+            response.Headers.Add("Content-Type", "text/csv; charset=utf-8");
+            response.WriteString(csv.ToString());
             return response;
-
-
         }
     }
 }
---
+++ OptionsData/EodOptions.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OptionsApi;
+public class EodOptions
+{
+    // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+    public class Call
+    {
+        [JsonPropertyName("contractName")]
+        public string ContractName { get; set; }
+
+        [JsonPropertyName("contractSize")]
+        public string ContractSize { get; set; }
+
+        [JsonPropertyName("contractPeriod")]
+        public string ContractPeriod { get; set; }
+
+        [JsonPropertyName("currency")]
+        public string Currency { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("inTheMoney")]
+        public string InTheMoney { get; set; }
+
+        [JsonPropertyName("lastTradeDateTime")]
+        public string LastTradeDateTime { get; set; }
+
+        [JsonPropertyName("expirationDate")]
+        public string ExpirationDate { get; set; }
+
+        [JsonPropertyName("strike")]
+        public double? Strike { get; set; }
+
+        [JsonPropertyName("lastPrice")]
+        public double? LastPrice { get; set; }
+
+        [JsonPropertyName("bid")]
+        public double? Bid { get; set; }
+
+        [JsonPropertyName("ask")]
+        public double? Ask { get; set; }
+
+        [JsonPropertyName("change")]
+        public double? Change { get; set; }
+
+        [JsonPropertyName("changePercent")]
+        public double? ChangePercent { get; set; }
+
+        [JsonPropertyName("volume")]
+        public int? Volume { get; set; }
+
+        [JsonPropertyName("openInterest")]
+        public int? OpenInterest { get; set; }
+
+        [JsonPropertyName("impliedVolatility")]
+        public double? ImpliedVolatility { get; set; }
+
+        [JsonPropertyName("delta")]
+        public double? Delta { get; set; }
+
+        [JsonPropertyName("gamma")]
+        public double? Gamma { get; set; }
+
+        [JsonPropertyName("theta")]
+        public double? Theta { get; set; }
+
+        [JsonPropertyName("vega")]
+        public double? Vega { get; set; }
+
+        [JsonPropertyName("rho")]
+        public double? Rho { get; set; }
+
+        [JsonPropertyName("theoretical")]
+        public double? Theoretical { get; set; }
+
+        [JsonPropertyName("intrinsicValue")]
+        public double? IntrinsicValue { get; set; }
+
+        [JsonPropertyName("timeValue")]
+        public double? TimeValue { get; set; }
+
+        [JsonPropertyName("updatedAt")]
+        public string UpdatedAt { get; set; }
+
+        [JsonPropertyName("daysBeforeExpiration")]
+        public int? DaysBeforeExpiration { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonPropertyName("expirationDate")]
+        public string ExpirationDate { get; set; }
+
+        [JsonPropertyName("impliedVolatility")]
+        public double? ImpliedVolatility { get; set; }
+
+        [JsonPropertyName("putVolume")]
+        public int? PutVolume { get; set; }
+
+        [JsonPropertyName("callVolume")]
+        public int? CallVolume { get; set; }
+
+        [JsonPropertyName("putCallVolumeRatio")]
+        public double? PutCallVolumeRatio { get; set; }
+
+        [JsonPropertyName("putOpenInterest")]
+        public int? PutOpenInterest { get; set; }
+
+        [JsonPropertyName("callOpenInterest")]
+        public int? CallOpenInterest { get; set; }
+
+        [JsonPropertyName("putCallOpenInterestRatio")]
+        public double? PutCallOpenInterestRatio { get; set; }
+
+        [JsonPropertyName("optionsCount")]
+        public int? OptionsCount { get; set; }
+
+        [JsonPropertyName("options")]
+        public Options Options { get; set; }
+    }
+
+    public class Options
+    {
+        [JsonPropertyName("Call")]
+        public List<Call> Call { get; set; }
+
+        [JsonPropertyName("Put")]
+        public List<Put> Put { get; set; }
+    }
+
+    public class Put
+    {
+        [JsonPropertyName("contractName")]
+        public string ContractName { get; set; }
+
+        [JsonPropertyName("contractSize")]
+        public string ContractSize { get; set; }
+
+        [JsonPropertyName("contractPeriod")]
+        public string ContractPeriod { get; set; }
+
+        [JsonPropertyName("currency")]
+        public string Currency { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("inTheMoney")]
+        public string InTheMoney { get; set; }
+
+        [JsonPropertyName("lastTradeDateTime")]
+        public string LastTradeDateTime { get; set; }
+
+        [JsonPropertyName("expirationDate")]
+        public string ExpirationDate { get; set; }
+
+        [JsonPropertyName("strike")]
+        public double? Strike { get; set; }
+
+        [JsonPropertyName("lastPrice")]
+        public double? LastPrice { get; set; }
+
+        [JsonPropertyName("bid")]
+        public double? Bid { get; set; }
+
+        [JsonPropertyName("ask")]
+        public double? Ask { get; set; }
+
+        [JsonPropertyName("change")]
+        public double? Change { get; set; }
+
+        [JsonPropertyName("changePercent")]
+        public double? ChangePercent { get; set; }
+
+        [JsonPropertyName("volume")]
+        public int? Volume { get; set; }
+
+        [JsonPropertyName("openInterest")]
+        public int? OpenInterest { get; set; }
+
+        [JsonPropertyName("impliedVolatility")]
+        public double? ImpliedVolatility { get; set; }
+
+        [JsonPropertyName("delta")]
+        public double? Delta { get; set; }
+
+        [JsonPropertyName("gamma")]
+        public double? Gamma { get; set; }
+
+        [JsonPropertyName("theta")]
+        public double? Theta { get; set; }
+
+        [JsonPropertyName("vega")]
+        public double? Vega { get; set; }
+
+        [JsonPropertyName("rho")]
+        public double? Rho { get; set; }
+
+        [JsonPropertyName("theoretical")]
+        public double? Theoretical { get; set; }
+
+        [JsonPropertyName("intrinsicValue")]
+        public double? IntrinsicValue { get; set; }
+
+        [JsonPropertyName("timeValue")]
+        public double? TimeValue { get; set; }
+
+        [JsonPropertyName("updatedAt")]
+        public string UpdatedAt { get; set; }
+
+        [JsonPropertyName("daysBeforeExpiration")]
+        public int? DaysBeforeExpiration { get; set; }
+    }
+
+    public class EodOptionsResponse
+    {
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+
+        [JsonPropertyName("exchange")]
+        public string Exchange { get; set; }
+
+        [JsonPropertyName("lastTradeDate")]
+        public string LastTradeDate { get; set; }
+
+        [JsonPropertyName("lastTradePrice")]
+        public double? LastTradePrice { get; set; }
+
+        [JsonPropertyName("data")]
+        public List<Data> Data { get; set; }
+    }
+
+
+}
